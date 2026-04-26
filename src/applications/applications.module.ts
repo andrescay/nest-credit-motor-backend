ee29c4ac747cc9cf418e888529c77790f28622f5
Cristian Andres Alvarez Yaya <hsk@MacBook-Pro-de-Cristian.local>
2026-04-26 08:32:47 -0500

    feat(applications): add service|module|servivice-spec

--- src/applications/applications.module.ts
+++ src/applications/applications.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ApplicationsController } from './applications.controller';
+import { ApplicationsService } from './applications.service';
 
-@Module({})
+@Module({
+  controllers: [ApplicationsController],
+  providers: [ApplicationsService]
+})
 export class ApplicationsModule {}
